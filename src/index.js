eeaba2f94176a8aa56d99d7e842780e02319877b
[Valentina] <[mariaiscus1@gmail.com]>
2021-08-10 19:42:09 +0600

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+  const dictBrackets = Object.fromEntries(bracketsConfig)
+
+  for (const bracket of str) {
+    if (bracket === dictBrackets[stack[stack.length - 1]]) {
+      stack.pop()
+    } else if (dictBrackets.hasOwnProperty(bracket)) {
+      stack.push(bracket)
+    }
+    else if (!dictBrackets.hasOwnProperty(bracket)) {
+      return false
+    }
+  }
+  return stack.length === 0
 }
