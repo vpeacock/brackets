f611dddf4d023b5323a3821498e0dca732f61f86
[Valentina] <[mariaiscus1@gmail.com]>
2022-01-27 15:33:21 +0600

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  let stack = []
-  const dictBrackets = Object.fromEntries(bracketsConfig)
+module.exports = function check (str, bracketsConfig) {
+  if(str.length % 2 != 0) return false;
+  
+  let stack = [];
+  const objBrackets = Object.fromEntries(bracketsConfig);
 
-  for (const bracket of str) {
-    if (bracket === dictBrackets[stack[stack.length - 1]]) {
-      stack.pop()
-    } else if (dictBrackets.hasOwnProperty(bracket)) {
-      stack.push(bracket)
-    }
-    else if (!dictBrackets.hasOwnProperty(bracket)) {
-      return false
-    }
+  for (let i = 0; i < str.length; i++) {
+    if (objBrackets[stack[stack.length - 1]] === str[i]) {
+      stack.pop();
+    } else if (Object.keys(objBrackets).includes(str[i])) {
+      stack.push(str[i]);
+    } 
   }
-  return stack.length === 0
-}
+
+  return stack.length === 0;
+};
